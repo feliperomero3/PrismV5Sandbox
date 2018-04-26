40bfb72668d3968e8edf9587eaa19518a71d7ef5
Felipe Romero <feliperomero3@outlook.com>
2018-04-26 16:03:01 -0600

    Agregar "Hello World".

---
+++ PrismHelloWorld/Bootstrapper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Practices.Prism.UnityExtensions;
+using Microsoft.Practices.Unity;
+
+namespace PrismV5Sandbox
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+
+            App.Current.MainWindow = (Window)Shell;
+            App.Current.MainWindow.Show();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# PrismV5 Sandbox
+
+Uso básico de la librearía Prism desarrollada por Microsoft Patterns & Practices.
+
+## Instrucciones
+
+- Clonar el proyecto 
+- Restaurar nuget packages
+- Iniciar aplicación
+
+## Proyectos dentro de la solución
+
+### PrismHelloWorld
+
+Aplicación tipo "Hello World". Mostrar un solo bloque de texto.
